0f01f190a0c17d08dfcf49125ee69635f086a135
Shubham Jain <luckyj5@users.noreply.github.com>
2018-02-12 14:45:32 -0800

    added configurable java heap size

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #Delay startup to make sure zookeeper is up and running 
 sleep 30
+export KAFKA_HEAP_OPTS=$KAFKA_HEAP_OPTS
 /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/bin/kafka-server-start.sh /opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"/config/server.properties
